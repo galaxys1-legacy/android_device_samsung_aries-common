cfb4b7dbb49e13f4ec71d6ba617e02befe00704f
Cswl Coldwind <cswl1337@gmail.com>
2017-11-02 14:35:45 +0545

    seccomp_policy: add mediaextractor policy
    Change-Id: I198395cc793baf30bd2204b18e29619ca41d3348

--- device_base.mk
+++ device_base.mk
@@ -277,7 +277,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
 # HIDL
 PRODUCT_COPY_FILES += \
