865801caa61af148300f78d1a41ae44037c0c9e4
Cswl Coldwind <cswl1337@gmail.com>
2017-11-10 20:13:27 +0545

    [nougat-only] disabled Oreo specific product makefiles
    Change-Id: I5798b0a801c0b5f9663878ecac00f0b2faf29562

--- product/state_of_the_art.mk
+++
@@ -1,8 +0,0 @@
-# Shiny new shits in Android O
-
-DEVICE_COMMON := device/samsung/aries-common
-
-# Seccomp policy
-PRODUCT_COPY_FILES += \
-    $(DEVICE_COMMON)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(DEVICE_COMMON)/seccomp_policy/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
