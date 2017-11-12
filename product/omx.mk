357a2ae598104960121acf14b5a5d34ec2ce0402
Cswl Coldwind <cswl1337@gmail.com>
2017-11-12 09:20:20 +0545

    omx: remove Oreo prop, fix naming of google codec
    Change-Id: Id27dd1967634f106fe7d5e8dbae73c8d703fc0fb

--- product/omx.mk
+++ product/omx.mk
@@ -1,7 +1,3 @@
-# Default OMX service to non-Treble
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.media.treble_omx=false
-
 # OpenMAX IL modules
 PRODUCT_PACKAGES += \
     libSEC_OMX_Core \
@@ -16,5 +12,5 @@ PRODUCT_COPY_FILES += \
     device/samsung/aries-common/config/media_codecs.xml:system/etc/media_codecs.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml \
     hardware/samsung-exynos3/s5pc110/sec_mm/sec_omx/sec_omx_core/secomxregistry:system/etc/secomxregistry
