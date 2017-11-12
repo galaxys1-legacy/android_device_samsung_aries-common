5fe13f69d950fcdd461eafa946b6a360f5ac2f68
Cswl Coldwind <cswl1337@gmail.com>
2017-11-12 09:15:49 +0545

    [nougat-only] enable cmhw
    Change-Id: I8489dc7ba128b8de4fd230a9fd07b220b15baf54

--- board/misc.mk
+++ board/misc.mk
@@ -8,10 +8,13 @@ TARGET_INCREASES_COLDBOOT_TIMEOUT := true
 BOARD_HAS_BLN := true
 
 # Include an expanded selection of fonts
- EXTENDED_FONT_FOOTPRINT := true
+EXTENDED_FONT_FOOTPRINT := true
 
 # CMHW
-# BOARD_HARDWARE_CLASS := device/samsung/aries-common/cmhw
+BOARD_USES_CYANOGEN_HARDWARE := true
+BOARD_HARDWARE_CLASS += hardware/cyanogen/cmhw \
+   device/samsung/aries-common/cmhw
+ 
 
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/aries-common
