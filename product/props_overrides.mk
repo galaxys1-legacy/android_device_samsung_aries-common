23a9da45c024591d017ab2a1b96af2eba5291362
Cswl Coldwind <cswl1337@gmail.com>
2017-11-12 09:20:58 +0545

    props: commonize and organize props
    Change-Id: I095ef449aa76a1f8dcf732b6aa9e81ae5207aea5

--- product/props_overrides.mk
+++ product/props_overrides.mk
@@ -2,14 +2,17 @@
 # Note that the only such settings should be the ones that are too low-level to
 # be reachable from resources or other mechanisms.
 
+# Connectivity
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.media.legacy-drm=1 \
     wifi.interface=wlan0 \
     mobiledata.interfaces=pdp0,eth0,gprs,ppp0,rmnet \
-    ro.bq.gpu_to_cpu_unsupported=1 \
     ro.ril.hsxpa=1 \
     ro.ril.gprsclass=10 \
+
+# Low RAM Optimizations
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.low_ram=true \
+    ro.config.max_starting_bg=8 \
     ro.sys.fw.bg_apps_limit=8 \
     ro.ksm.default=1
 
@@ -17,8 +20,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-flags=--no-watch-dog \
     dalvik.vm.image-dex2oat-filter=speed \
-	dalvik.vm.dex2oat-filter=speed
-
+    dalvik.vm.dex2oat-filter=speed
 # Force dex2oat to not use swap file
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-swap=false
@@ -28,10 +30,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     camera2.portability.force_api=1 \
     camera2.port.operation_time_ms=5000 \
     persist.camera.HAL3.enabled=0
-
-# SGX540 is slower with the scissor optimization enabled
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.disable_scissor_opt=true
 
 # Enable Google-specific location features,
 # like NetworkLocationProvider and LocationCollector
@@ -50,7 +48,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Default=true for development builds, set by android buildsystem.
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.kernel.android.checkjni=0 \
-    dalvik.vm.checkjni=false
+    dalvik.vm.checkjni=false \
+    ro.config.nocheckin=1
     
 # Strict mode disabled
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -60,6 +59,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # OpenGL
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.hwui.render_dirty_regions=false \
+    ro.bq.gpu_to_cpu_unsupported=1 \
     ro.hwui.r_buffer_cache_size=1 \
     ro.hwui.layer_cache_size=8 \
     ro.hwui.path_cache_size=2 \
@@ -68,6 +68,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.hwui.texture_cache_size=12 \
     ro.opengles.version=131072 \
     ro.zygote.disable_gl_preload=true
+# SGX540 is slower with the scissor optimization enabled
+# EGL blobs crash on screen off
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.disable_scissor_opt=true \
+    ro.egl.destroy_after_detach=true
 
 # System server
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -76,6 +81,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.media.legacy-drm=1 \
     media.stagefright.legacyencoder=true \
 	media.stagefright.less-secure=true
 
