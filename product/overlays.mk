f882b98f6928be710c27c662b7740a77c0b97e12
Cswl Coldwind <cswl1337@gmail.com>
2017-11-15 20:35:53 +0545

    overlays: unify and re-order overlays
    Change-Id: Ibefe825b5d20bd55ade2a12b1d0250d94e71e425

--- product/overlays.mk
+++ product/overlays.mk
@@ -1,4 +1,4 @@
 # These is the hardware-specific overlay, which points to the location
 # of hardware-specific resource overrides, typically the frameworks and
 # application settings that are stored in resourced.
-DEVICE_PACKAGE_OVERLAYS += device/samsung/aries-common/overlay
+DEVICE_PACKAGE_OVERLAYS := device/samsung/aries-common/overlay
